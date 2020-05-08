bfe8bbad1cd5cb847a5f0248e3831d7a3aab25d2
RomarioBennett <Romario_bennett@outlook.com>
2020-05-08 15:30:42 -0500

    May 8

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -31,8 +31,10 @@ migrate = Migrate(app,db)
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
 from flaskblog.main.routes import main
+from flaskblog.errors.handlers import errors
+
 
 app.register_blueprint(users)
 app.register_blueprint(posts)
 app.register_blueprint(main)
-
+app.register_blueprint(errors)
